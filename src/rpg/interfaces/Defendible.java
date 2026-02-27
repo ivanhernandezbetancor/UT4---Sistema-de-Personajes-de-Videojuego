78130e262beddaafe42fd769e91fc6decbf30486
Estefanía <estefaniacarrillosuarez@gmail.com>
2026-02-27 14:10:48 +0000

    Actualizar Defendible

--- src/rpg/interfaces/Defendible.java
+++ src/rpg/interfaces/Defendible.java
@@ -1,13 +1,11 @@
 package rpg.interfaces;
 
 // Interfaz que representa la capacidad de defenderse
-
 public interface Defendible {
+    
     // Acción de defensa 
-
     void defender();
 
     // Método para reducir el daño recibido
-    
     int reducirDanio(int danioEntrante);
 }
